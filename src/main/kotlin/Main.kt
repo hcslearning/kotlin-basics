6baa37a80244fc879d3fddfe059444615f56b233
hcslearning <learning@hardcybersoft.cl>
2022-06-15 11:55:52 -0400

    Probando método estático (companion object)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 import model.Persona
+import usecase.CollectionObjectToString
+import usecase.CustomGetterSetter
 
 fun main(args: Array<String>) {
 
+    //CollectionObjectToString.main() // estático companion object
+
+    CustomGetterSetter().main() // instanciable
 }
--- src/main/kotlin/usecase/CollectionObjectToString.kt
+++ src/main/kotlin/usecase/CollectionObjectToString.kt
@@ -3,12 +3,17 @@ package usecase
 import model.Persona
 
 class CollectionObjectToString {
-    fun main() {
-        val personas = listOf<Persona>(Persona("Juan"), Persona("Pedro"), Persona("María"))
 
-        val nombres = personas
-            .map { it.nombre }
-            .joinToString(",")
-        println(nombres)
+    // static fun
+    companion object {
+        fun main() {
+            val personas = listOf<Persona>(Persona("Juan"), Persona("Pedro"), Persona("María"))
+
+            val nombres = personas
+                .map { it.nombre }
+                .joinToString(",")
+            println(nombres)
+        }
     }
+
 }
